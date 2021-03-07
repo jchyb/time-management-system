2b32ba7fef0b697a27f3ff38bbad3973bf2380d4
Jan <jan.chyb.99@gmail.com>
2021-03-07 15:13:04 +0100

    Partially changed controller to asynchronous

--- app/models/TaskDAO.scala
+++ app/models/TaskDAO.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.atomic.AtomicLong
 /*
 Business logic, will be replaced by a persistent solution.
  */
-case class Task(name: String, id: Int)
+case class Task(name: String, userID: Int)
 object TaskCollection {
 
     private val tasks = TrieMap.empty[String, Task]
@@ -13,13 +13,13 @@ object TaskCollection {
     def list(): List[Task] = tasks.values.toList
     def create(name: String): Option[Task] = {
       val id = seq.incrementAndGet()
-      val item = Task(name, id.toInt)
+      val item = Task(name, 0)
       tasks.put(name, item)
       Some(item)
     }
     def get(name: String): Option[Task] = tasks.get(name)
-    def update(id: Int, name: String, price: Double): Option[Task] = {
-      val item = Task(name, id)
+    def update(userID: Int, name: String): Option[Task] = {
+      val item = Task(name, 0)
       tasks.replace(name, item)
       Some(item)
     }

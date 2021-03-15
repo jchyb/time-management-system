f70170dc15c88fb23b5eea9e2d2eab1346e5f91c
Jan <jan.chyb.99@gmail.com>
2021-03-15 13:47:33 +0100

    added operations on application start up

--- app/modules/ApplicationStart.scala
+++ app/modules/ApplicationStart.scala
@@ -1,5 +1,5 @@
 package modules
-import business.database.Sessions
+import business.database.{Sessions, Tasks, Users}
 
 import scala.concurrent.{ExecutionContext, Future}
 import javax.inject._
@@ -7,12 +7,19 @@ import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.inject.ApplicationLifecycle
 import slick.jdbc.JdbcProfile
 import slick.lifted.TableQuery
+import slick.jdbc.PostgresProfile.api._
 
+@Singleton
 class ApplicationStart @Inject()(lifecycle: ApplicationLifecycle, protected val dbConfigProvider: DatabaseConfigProvider)(
   implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
 
   //create table
   val sessions = TableQuery[Sessions]
+  val users = TableQuery[Users]
+  val tasks = TableQuery[Tasks]
+
+  db.run((sessions.schema ++ users.schema ++ tasks.schema).createIfNotExists)
+
 
   // Shut-down hook
   lifecycle.addStopHook { () =>
